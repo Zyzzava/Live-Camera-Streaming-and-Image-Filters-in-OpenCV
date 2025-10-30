e511ec835cfa6b35dd3f4b8300a60652ce0676a5
Mads Pagh <mads.p.pagh@gmail.com>
2025-10-30 20:01:20 +0100

    added finishing touches

--- main.cpp
+++ main.cpp
@@ -28,8 +28,10 @@ int main()
     }
 
     // glfw setup
+    // Setting OpenGL version to 3.3 Core Profile
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // Setting OpenGL profile
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // needed for mac
 
